e20d4404766cbcb262f7ddc9fb08b0c085ca908c
oucief <dahaoucif@gmail.com>
2023-10-08 17:49:27 +0100

    Add Particles to backgroun

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Particle from './components/particle/Particle';
 import Navigation from './components/navigation/Navigation';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/image-link-form/ImageLinkForm';
@@ -7,6 +7,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+      <Particle />
       <Navigation /> 
       <Rank />
       <ImageLinkForm/>
